3c5df9a59ae1e1a98346d56e181cce20119f86d0
gretiv <gretty121@gmail.com>
2018-05-12 16:42:56 +0300

    added login controller

---
+++ WAD Project/src/java/Controllers/LoginController.java
@@ -0,0 +1,115 @@
+package Controllers;
+
+import DAO.UserDAO;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Radu
+ */
+@WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
+public class LoginController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+
+        Enumeration en = request.getParameterNames();
+        List<String> errors = new ArrayList<>();
+        String uname = null;
+        String pass = null;
+        try {
+            while (en.hasMoreElements()) {
+                Object objOri = en.nextElement();
+                String param = (String) objOri;
+                String value = request.getParameter(param);
+                if ("uname".equals(param)) {
+                    uname = value;
+                }
+                if ("password".equals(param)) {
+                    pass = value;
+                }
+            }
+
+            if (!UserDAO.getInstance().login(uname, pass)) {
+                request.setAttribute("user_session", null);
+                errors.add("Invalid username/password");
+                request.setAttribute("errors", errors);
+                request.getRequestDispatcher("LoginView.jsp").forward(request, response);
+            } else if(UserDAO.getInstance().login(uname,pass)){
+                request.getSession().setAttribute("user_session", uname);
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
